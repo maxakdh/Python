689369c2d9e941f064d614e5f7255a24208ac962
maxakdh <85213643+maxakdh@users.noreply.github.com>
2021-06-07 21:19:13 -0700

    Add files via upload

---
+++ Python for Everyone/ex_04_06.py
@@ -0,0 +1,18 @@
+def computepay (hours, rate) :
+    #print("In computepay",  hours, rate)
+    if hours > 40:
+        reg = rate * 40
+        otp = (hours - 40.0) * (rate * 1.5)
+        pay = reg + otp
+    else:
+        pay = hours * rate
+        #print("Returning", pay)
+        return pay
+
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+fh = float(sh)
+fr = float (sr)
+xp = computepay(fh, fr)
+
+print("Pay",xp)
